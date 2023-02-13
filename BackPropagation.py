8ef3a238c4ef23ccd7a4e2ede0dd77332264a4c3
whatabouttania <a2003tania@gmail.com>
2023-02-14 00:17:05 +0200

    add fit method to BackPropagation class

--- BackPropagation.py
+++ BackPropagation.py
@@ -1,20 +1,38 @@
 class BackPropagation:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self):
+        self.X = None
+        self.y = None
 
-    @property
-    def data(self):
-        return self.__data
-
-    @data.setter
-    def data(self, value):
-        if not isinstance(value, list):
-            raise TypeError
-        if not len(value):
-            raise ValueError
-        self.__data = value
-
-    def predict(self):
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
         pass
 
+    @property
+    def X(self):
+        return self.__X
+
+    @X.setter
+    def X(self, value):
+        if not isinstance(value, list):
+            raise TypeError
+        if not all(isinstance(v, (int, float)) for v in value):
+            raise TypeError
+        if len(value) != 3:
+            raise ValueError
+        self.__X = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError
+        self.__y = value
+
+    def predict(self, X):
+        self.X = X
+        pass
 
