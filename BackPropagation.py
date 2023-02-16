5785d16ceff5ab685c67e592f3a949c3fedb3ff5
whatabouttania <a2003tania@gmail.com>
2023-02-16 08:45:09 +0200

    implement back propagation to the step of finding new weights

--- BackPropagation.py
+++ BackPropagation.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from NeuralNetwork import NeuralNetwork
-from functions import weighted_sum, sigmoid
+from functions import weighted_sum, sigmoid, sigmoid_derivative
 
 
 class BackPropagation:
@@ -14,35 +14,50 @@ class BackPropagation:
         self.X = X
         self.y = y
         for epoch in range(0, max_epochs):
-          for X_train, y_train in zip(X, y):
-              print('---')
-              print(X_train)
-              self.__forward(X_train, y_train)
+            for X_train, y_train in zip(X, y):
+                result = self.__forward(X_train, y_train)
+                y_output = result[-1][0]
 
-          print('epoch: {}\t'.format(epoch))
+                print('actual: {}\t\t\t\texpected: {}\t'.format(y_output, y_train))
+
+                self.__back(Y=y_output, y=y_train, all_outs=result)
+            print('epoch: {}\t'.format(epoch))
 
     def __forward(self, X, y):
         # outputs calculated with activation func on current layer
         outputs = []
         # iterate through layers of neural network
-        for layer in range(0 , self.neural_network.layers_num):
-            print('in layers')
+        for layer in range(0, self.neural_network.layers_num):
             # outputs on current layer
             outputs_ = []
             # iterate through weight vectors of current layer
             for w in self.neural_network.weights[layer]:
-                print('\tin weights')
-                print(X)
                 # calculate weighted sum (dot product)
                 S = (weighted_sum(X, w))
                 # activation func
-                F = sigmoid(S)
-                outputs_.append(F)
-                print('\t\t'+str(S))
-                print('\t\t'+str(F))
+                Y = sigmoid(S)
+                outputs_.append(Y)
             X = outputs_
             outputs.append(outputs_)
-        print(outputs)
+        return outputs
+
+    def __back(self, Y, y, all_outs):
+        delta = Y - y
+        Y_ = sigmoid_derivative(Y)
+        for layer in range(0, self.neural_network.layers_num-1):
+            # calculate partial derivative of the error func for each output in layer
+            # and store all results in list
+            E_ = np.dot(delta * Y_, all_outs[layer])
+            print('Weights before:')
+            print(self.neural_network.weights[layer])
+            for (w_i, w), e_ in zip(enumerate(self.neural_network.weights[layer]), E_):
+                print(str(w_i) + ' ' + str(w) + ' ' + str(e_))
+                self.neural_network.weights[layer][w_i] -= self.neural_network.learning_rate * e_
+            print('Weights after:')
+            print(self.neural_network.weights[layer])
+
+
+
 
     def predict(self, X):
         self.X = X
