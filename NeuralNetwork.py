fea422dc4e1784595694c782c79cc7d63341ca29
whatabouttania <a2003tania@gmail.com>
2023-02-17 23:15:40 +0200

    change shape of weight matrix

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -6,6 +6,7 @@ class NeuralNetwork:
         self.nn_structure = nn_structure
         self.layers_num = len(nn_structure) - 1
         self.learning_rate = learning_rate
-        self.weights = [np.random.normal(scale=1, size=(self.nn_structure[i + 1],
-                                                        self.nn_structure[i]))
+        self.weights = [np.random.normal(scale=1, size=(self.nn_structure[i],
+                                                        self.nn_structure[i + 1]))
                         for i in range(0, self.layers_num)]
+
